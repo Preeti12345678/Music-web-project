42ab850f15304d449040d862bd9a85efe2f1139d
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-06-18 14:47:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 score_leftWrist=0;
-song_status="";
+score_rightWrist=0;
+song1_status="";
+song2_status="";
 function preload(){
     butter=loadSound("butter.mp3");
     on_the_ground=loadSound("on the ground.mp3");
@@ -20,6 +22,8 @@ function setup(){
 }
 function play(){
     butter.play();
+    butter.setVolume(1);
+    butter.rate(1);
 }
 function modelLoaded(){
     console.log("Model Loaded");
@@ -28,6 +32,7 @@ function gotposes(results){
     if(results.length>0){
         console.log(results);
         score_leftWrist=results[0].pose.keypoints[9].score;
+        score_rightWrist=results[0].pose.keypoints[10].score;
         console.log("Score= "+score_leftWrist);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
@@ -39,15 +44,24 @@ function gotposes(results){
 }
 function draw(){
     image(video,0,0,400,300);
-    song_status=butter.isPlaying();
+    song1_status=butter.isPlaying();
+    song2_status=on_the_ground.isPlaying();
     fill("red");
     stroke("red");
-    if(score_leftWrist>0.02){
+    if(score_leftWrist>0.2){
         circle(leftWristX,leftWristY,20);
         on_the_ground.stop();
-        if(song_status=="false"){
+        if(song1_status=="false"){
             butter.play();
             document.getElementById("song").innerHTML=" =Butter";
+        }
+    }
+    if(score_rightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        butter.stop();
+        if(song2_status=="false"){
+            on_the_ground.play();
+            document.getElementById("song").innerHTML=" =On the ground";
         }
     }
 }
